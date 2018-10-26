a339f0cc3014fbbbef66f2c6a5871501f151a811
Nóra Giczy <zynori@gmail.com>
2018-10-26 17:53:02 +0200

    feat(bookstore): create app

---
+++ AntiqueStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookTitle { get; set; }
+        public int BookQuantity { get; set; }
+        public int BookReleased { get; set; }
+        public string BookPublisher { get; set; }
+        public int BookPages { get; set; }
+        public int? BookSeries { get; set; } 
+
+        public BookSeries BookSeries { get; set; }
+    }
+}
---
+++ AntiqueStore/Models/BookSeries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Models
+{
+    public class BookSeries
+    {
+        public int BookSeriesId { get; set; }
+        public string BookSeriesTitle { get; set; }
+        public int BookSeriesPart { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
