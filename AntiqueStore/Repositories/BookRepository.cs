d15fcc1665a7ae2e00c80ed9ee618220c6d0585c
Nóra Giczy <zynori@gmail.com>
2018-10-27 12:46:49 +0200

    feat: add crud

---
+++ AntiqueStore/Repositories/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using AntiqueStore.Entities;
+using AntiqueStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Repositories
+{
+    public class BookRepository : IGenericRepository<Book>
+    {
+        BookContext database;
+
+        public BookRepository(BookContext database)
+        {
+            this.database = database;
+        }
+
+        public void Create(Book book)
+        {
+            database.Books.Add(book);
+            database.SaveChanges();
+        }
+
+        public void Delete(Book book)
+        {
+            database.Books.Remove(book);
+            database.SaveChanges();
+        }
+
+        public List<Book> Read()
+        {
+            return database.Books.ToList();
+        }
+
+        public void Update(Book book)
+        {
+            database.Books.Update(book);
+            database.SaveChanges();
+        }
+    }
+}
