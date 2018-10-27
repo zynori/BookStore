2bffb249c3fa56f1edf1d5482172adcbde5c6b9b
Nóra Giczy <zynori@gmail.com>
2018-10-27 12:40:20 +0200

    feat: add generic repository

---
+++ AntiqueStore/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Repositories
+{
+    public interface IGenericRepository<Object>
+    {
+        void Create(Object element);
+        List<Object> Read();
+        void Update(Object element);
+        void Delete(Object element);
+    }
+}
