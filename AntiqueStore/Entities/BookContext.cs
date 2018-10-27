771c2a7f549cfecb28f993b7d587b0812b1f5c70
Nóra Giczy <zynori@gmail.com>
2018-10-27 10:44:55 +0200

    feat: add entity

---
+++ AntiqueStore/Entities/BookContext.cs
@@ -0,0 +1,21 @@
+﻿using AntiqueStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Entities
+{
+    public class BookContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Dimension> Dimensions { get; set; }
+        public virtual DbSet<Format> Formats { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+    }
+}
