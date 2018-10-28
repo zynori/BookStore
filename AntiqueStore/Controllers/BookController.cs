65794863307f0a77f9263420252683a135491e5c
Nóra Giczy <zynori@gmail.com>
2018-10-28 12:51:31 +0100

    feat: add controller func

--- AntiqueStore/Controllers/BookController.cs
+++ AntiqueStore/Controllers/BookController.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AntiqueStore.Models;
+using AntiqueStore.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AntiqueStore.Controllers
 {
     public class BookController : Controller
     {
+
+        BookService db;
+
+        public BookController(BookService db)
+        {
+            this.db = db;
+        }
+
         [HttpGet("/books")]
         public IActionResult Index()
         {
+            return View(db.ReadAll());
+        }
+
+        [HttpGet("/add")]
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost("/add")]
+        public IActionResult AddNew(Book book)
+        {
+            db.Add(book);
+            return RedirectToAction("/books");
         }
     }
 }
