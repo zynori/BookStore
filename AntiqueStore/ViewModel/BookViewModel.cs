5b19a2e9b165650bdc81ceb08bb3d96a03b17133
Nóra Giczy <zynori@gmail.com>
2018-10-29 12:56:23 +0100

    feat: add ViewModel

---
+++ AntiqueStore/ViewModel/BookViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AntiqueStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.ViewModel
+{
+    public class BookViewModel
+    {
+        public List<Book> Books { get; set; }
+        public List<Format> Formats { get; set; }
+        public List<Quality> Qualities { get; set; }
+
+        public BookViewModel()
+        {
+            Books = new List<Book>();
+            Formats = new List<Format>();
+            Qualities = new List<Quality>();
+        }
+    }
+}
