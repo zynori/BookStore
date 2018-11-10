b363bf19d4c6c271f2d1f796d002ce7ab9a54684
Nóra Giczy <zynori@gmail.com>
2018-11-10 16:06:58 +0100

    feat: add customer crud

---
+++ AntiqueStore/Repositories/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using AntiqueStore.Entities;
+using AntiqueStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiqueStore.Repositories
+{
+    public class CustomerRepository : IGenericRepository<Customer>
+    {
+        BookContext database;
+
+        public CustomerRepository(BookContext database)
+        {
+            this.database = database;
+        }
+
+        public void Create(Customer customer)
+        {
+            database.Customers.Add(customer);
+            database.SaveChanges();
+        }
+
+        public void Delete(Customer customer)
+        {
+            database.Customers.Remove(customer);
+            database.SaveChanges();
+        }
+
+        public Customer GetRecordById(int id)
+        {
+            return Read().FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Customer> Read()
+        {
+            return database.Customers.ToList();
+        }
+
+        public void Update(Customer customer)
+        {
+            database.Customers.Update(customer);
+            database.SaveChanges();
+        }
+    }
+}
